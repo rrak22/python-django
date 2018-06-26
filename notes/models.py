5989d1958b11c14318a8325d18ded289335464fe
Robert Rak <robertrak@hotmail.com>
2018-06-26 14:06:47 -0400

    progress

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 class Note(models.Model):
@@ -9,4 +10,7 @@ class Note(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=20)
 
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
